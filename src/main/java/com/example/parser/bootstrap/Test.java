ade2c3e7bbf8a6d463e3625a4a5053abd6e3351f
Liubomyr680 <lshynkaruk8@gmail.com>
2020-06-28 12:13:46 +0300

    1)Remove "System.out.println" from code 2)Fix DB_list checking

--- src/main/java/com/example/parser/bootstrap/Test.java
+++ src/main/java/com/example/parser/bootstrap/Test.java
@@ -10,9 +10,7 @@ import org.springframework.stereotype.Component;
 public class Test implements CommandLineRunner  {
 
     private static final String filePath = "/Users/kristina/Downloads/dozvoli-na-vikidi-onovlenii.json";
-//    private PermitForEmissionsOfPollutants permit;
     private final PermitRepository permitRepository;
-
 
     public Test(PermitRepository permitRepository) {
         this.permitRepository = permitRepository;
@@ -21,43 +19,9 @@ public class Test implements CommandLineRunner  {
     @Override
     public void run(String... args) throws Exception {
 
-
         Parser parser = new Parser(permitRepository);
         parser.parser(filePath);
 
-
-
-//        FileReader reader = new FileReader(filePath);
-//        JSONParser jsonParser = new JSONParser();
-//        JSONArray jsonArray = (JSONArray) jsonParser.parse(reader);
-//
-////        for(int i=0; i<jsonArray.size(); i++)
-////            System.out.println("The " + i + " element of the array: " + jsonArray.get(i));
-//
-//        Iterator i = jsonArray.iterator();
-//
-//        while (i.hasNext()) {
-//
-//            JSONObject innerObj = (JSONObject) i.next();
-//            permit = new PermitForEmissionsOfPollutants();
-//
-//            permit.setEdrpou(String.valueOf(innerObj.get("edrpou")));
-//            permit.setNumber(String.valueOf(innerObj.get("number")));
-//            permit.setName(String.valueOf(innerObj.get("name")));
-//            permit.setData_type(String.valueOf(innerObj.get("data_type")));
-//            permit.setValidity(String.valueOf(innerObj.get("validity")));
-//            permit.setLegal_address(String.valueOf(innerObj.get("legal_address")));
-//            permit.setActual_address(String.valueOf(innerObj.get("actual_address")));
-//
-//            permitRepository.save(permit);
-//            System.out.println("edrpou "+ innerObj.get("edrpou") +
-//
-//                    " number " + innerObj.get("number") +
-//                    " name "  + innerObj.get("name") +
-//                    " data_type " + innerObj.get("data_type") +
-//                    " validity " + innerObj.get("validity") +
-//                    " legal_address " + innerObj.get("legal_address") +
-//                    "actual_address " + innerObj.get("actual_address")+"\n\n" );
         }
     }
 
--- src/main/java/com/example/parser/utils/Parser.java
+++ src/main/java/com/example/parser/utils/Parser.java
@@ -6,6 +6,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,13 +16,13 @@ import java.util.concurrent.TimeUnit;
 
 public class Parser {
 
-    // private static final String filePath = "C:\\Users\\Любомир\\Downloads\\dozvoli-na-vikidi-v-atmosferu-onovlenii.json";
     private PermitForEmissionsOfPollutants permit;
     private final PermitRepository permitRepository;
     private float time;
     private Date date;
     private int File_counter;
     private int DB_counter;
+    private static Logger log = LoggerFactory.getLogger(Parser.class);
 
     public Parser(PermitRepository permitRepository) {
         this.permitRepository = permitRepository;
@@ -39,46 +41,34 @@ public class Parser {
 
         Iterator i = jsonArray.iterator();
 
-        if(BD_list.size() == 0){
-            while (i.hasNext()) {
-                JSONObject innerObj = (JSONObject) i.next();
-                permit = new PermitForEmissionsOfPollutants();
+        while (i.hasNext()) {
+            JSONObject innerObj = (JSONObject) i.next();
+            permit = new PermitForEmissionsOfPollutants();
 
-                permit.setEdrpou(String.valueOf(innerObj.get("edrpou")));
-                permit.setNumber(String.valueOf(innerObj.get("number")));
-                permit.setName(String.valueOf(innerObj.get("name")));
-                permit.setData_type(String.valueOf(innerObj.get("data_type")));
-                permit.setValidity(String.valueOf(innerObj.get("validity")));
-                permit.setLegal_address(String.valueOf(innerObj.get("legal_address")));
-                permit.setActual_address(String.valueOf(innerObj.get("actual_address")));
-                permitRepository.save(permit);
-
-                JsonList.add(permit);
-                File_counter++;
-            }
+            permit.setEdrpou(String.valueOf(innerObj.get("edrpou")));
+            permit.setNumber(String.valueOf(innerObj.get("number")));
+            permit.setName(String.valueOf(innerObj.get("name")));
+            permit.setData_type(String.valueOf(innerObj.get("data_type")));
+            permit.setValidity(String.valueOf(innerObj.get("validity")));
+            permit.setLegal_address(String.valueOf(innerObj.get("legal_address")));
+            permit.setActual_address(String.valueOf(innerObj.get("actual_address")));
+            JsonList.add(permit);
+            File_counter++;
         }
-
-//        permit.setEdrpou("1");
-//        permit.setNumber("1");
-//        permit.setName("1");
-//        permit.setData_type("1");
-//        permit.setValidity("1");
-//        permit.setLegal_address("1");
-//        permit.setActual_address("1");
-//
-//        JsonList.add(permit);
 
         Set<PermitForEmissionsOfPollutants> union = new HashSet(JsonList);
         union.removeAll(BD_list);
 
-        for (PermitForEmissionsOfPollutants k : union)
+        for (PermitForEmissionsOfPollutants k : union) {
+            permitRepository.save(k);
             DB_counter++;
+        }
 
         long end = System.currentTimeMillis();
         time = TimeUnit.MILLISECONDS.toSeconds(end - start);
         date = new Date();
 
-        System.out.println("1) Затрачений час на обробку файлу " + time + " секунд" +
+        log.info("\n1) Затрачений час на обробку файлу " + time + " секунд" +
                 "\n2) Дата коли файл був оброблений " + date +
                 "\n3) Скільки записів вичитало з файлу " + File_counter +
                 "\n4) Скільки нових записів додало у базу " + DB_counter +
