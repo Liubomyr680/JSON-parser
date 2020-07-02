7e7c1ec70b35b5d3fe2f015052135fab033e62ac
Liubomyr680 <lshynkaruk8@gmail.com>
2020-07-02 14:13:30 +0300

    finish file download method

--- src/main/java/com/example/parser/controller/ParserController.java
+++ src/main/java/com/example/parser/controller/ParserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 public class ParserController {
 
     private String fileUrl = "https://data.gov.ua/dataset/94409436-a198-4b9d-9738-844405c5df94/resource/4ca1d5a9-543f-42a5-bc8c-d6fe5650be40/download/dozvoli-na-vikidi-onovlenii.json";
---
+++ src/main/resources/script.ddl
@@ -0,0 +1,13 @@
+create table json_data
+(
+	id int not null
+		primary key,
+	actual_address varchar(255) null,
+	data_type varchar(255) null,
+	edrpou varchar(255) null,
+	legal_address varchar(255) null,
+	name varchar(255) null,
+	number varchar(255) null,
+	validity varchar(255) null
+);
+
