ee5ca117e08c51f0b9d757d0aa284e50c3a7a013
Liubomyr680 <lshynkaruk8@gmail.com>
2020-07-29 10:11:02 +0300

    start doing number filter

--- src/main/java/com/example/parser/repository/PagingPermitRepository.java
+++ src/main/java/com/example/parser/repository/PagingPermitRepository.java
@@ -3,13 +3,22 @@ package com.example.parser.repository;
 import com.example.parser.entity.Permit;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PagingPermitRepository extends PagingAndSortingRepository<Permit, Long> {
 
     Page<Permit> findById(Pageable pageable, int id);
-    Page<Permit> findByNumber(Pageable pageable, String number);
+
+//    @Query(value = "SELECT * FROM json_data WHERE number LIKE '123%'",
+//            nativeQuery = true)
+//    Page<Permit> findByNumber(Pageable pageable, String number);
+
+    @Query(value = "SELECT * FROM json_data j WHERE j.number LIKE CONCAT('%',:number ,'%')",
+            nativeQuery = true)
+    Page<Permit> findByNumber(Pageable pageable, @Param("number") String number);
 
 }
